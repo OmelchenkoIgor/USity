0f1a298057dde394c2f99af8f0dbad5fc9b430b2
Illya <romanenkoisnot@gmail.com>
2024-05-25 00:04:47 +0300

    add: relevant widget

---
+++ src/app/widgets/relevant/relevant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relevant',
+  standalone: true,
+  imports: [],
+  templateUrl: 'relevant.component.html',
+  styleUrl: 'relevant.component.scss'
+})
+export class RelevantComponent {
+
+}
