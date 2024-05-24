17f8669207845f2bf21e6a88c5ed2b15104397c2
Illya <romanenkoisnot@gmail.com>
2024-05-25 00:05:18 +0300

    fix: implementation of new widgets

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../widgets/banner/banner.component';
 import { RecomendationsComponent } from '../../widgets/recomendations/recomendations.component';
+import { NewsComponent } from "../../widgets/news/news.component";
+import { RelevantComponent } from "../../widgets/relevant/relevant.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    BannerComponent,
-    RecomendationsComponent
-  ],
-  templateUrl: 'home.component.html',
-  styleUrl: 'home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: 'home.component.html',
+    styleUrl: 'home.component.scss',
+    imports: [
+        BannerComponent,
+        RecomendationsComponent,
+        NewsComponent,
+        RelevantComponent
+    ]
 })
 export default class HomeComponent {
 
