cbb7577925e7a197d3acff0941436846edb72106
OmelchenkoIgor <omelchenkoigor7777@gmail.com>
2024-05-25 16:16:25 +0300

    minor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../widgets/banner/banner.component';
 import { RecomendationsComponent } from '../../widgets/recomendations/recomendations.component';
-import { NewsComponent } from "../../widgets/news/news.component";
-import { RelevantComponent } from "../../widgets/relevant/relevant.component";
-import { AdvertisementComponent } from "../../widgets/advertisement/advertisement.component";
+import { NewsComponent } from '../../widgets/news/news.component';
+import { RelevantComponent } from '../../widgets/relevant/relevant.component';
+import { AdvertisementComponent } from '../../widgets/advertisement/advertisement.component';
 
 @Component({
     selector: 'app-home',
