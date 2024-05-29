f6b4767d0f33e283ea6e19eafa7c8d1abaa36632
Illya <romanenkoisnot@gmail.com>
2024-05-29 16:32:39 +0300

    add: all necessary widgets for partnership page

---
+++ src/app/widgets/partnership-page/infographics/infographics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infographics',
+  standalone: true,
+  imports: [],
+  templateUrl: 'infographics.component.html',
+  styleUrl: 'infographics.component.scss'
+})
+export class InfographicsComponent {
+
+}
